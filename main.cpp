90c005a36a90eefc83f7025f1332e6ac696b23d8
CristiYTRomania <163294932+CristiYTRomania@users.noreply.github.com>
2024-05-01 19:53:32 +0300

    Update main.cpp
    1 mai 2024

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 class Calculator
 {
@@ -50,94 +51,85 @@ public:
 };
 int main()
 {
-    int ok1=1,ok2;
-    while(ok1==1)
+inceput:
+    cout<<"Introduceti numarul 1 pentru numere intregi"<<endl;
+    cout<<"Introduceti numarul 2 pentru numere reale"<<endl;
+    cout<<"Introduceti orice alt numar intreg pentru terminarea programului"<<endl;
+    cout<<"Numarul introdus este: ";
+    int n;
+    Calculator myCalc;
+    long long a,b,c,d,e,f;
+    double g,h,i,j,k,l;
+    cin>>n;
+    if(n==1 /** or **/ || n==2)
+        cout<<"Introduceti 6 numere ";
+    if(n==1)
     {
-        ok2=1;
-        cout<<"Introduceti numarul 1 pentru numere intregi"<<endl;
-        cout<<"Introduceti numarul 2 pentru numere reale"<<endl;
-        cout<<"Introduceti orice alt numar intreg pentru terminarea programului"<<endl;
-        cout<<"Numarul introdus este: ";
-        int n;
-        Calculator myCalc;
-        long long a,b,c,d,e,f;
-        double g,h,i,j,k,l;
-        cin>>n;
-        if(n==1 /** or **/ || n==2)
-            cout<<"Introduceti 6 numere ";
-        if(n==1)
-        {
-            cout<<"intregi: ";
-            cin>>a>>b>>c>>d>>e>>f;
-            myCalc.declarareNumereIntregi(a,b,c,d,e,f);
-        }
-        else if(n==2)
-        {
-            cout<<"reale: ";
-            cin>>g>>h>>i>>j>>k>>l;
-            myCalc.declarareNumereReale(g,h,i,j,k,l);
-        }
-        else
-        {
-            ok1=0;
-            ok2=0;
-        }
-        while(ok2==1)
-        {
-            cout<<"Introduceti numarul 0 pentru reintroducerea numerelor"<<endl;
-            cout<<"Introduceti numarul 1 pentru adunarea celor 6 numere"<<endl;
-            cout<<"Introduceti numarul 2 pentru scaderea primului numar cu suma celorlalte 5 numere"<<endl;
-            cout<<"Introduceti numarul 3 pentru inmultirea celor 6 numere"<<endl;
-            cout<<"Introduceti numarul 4 pentru impartirea primului numar cu produsul celorlalte 5 numere"<<endl;
-            cout<<"Introduceti orice alt numar intreg pentru terminarea programului"<<endl;
-            cout<<"Numarul introdus este: ";
-            int m;
-            cin>>m;
-            if(m<0||m>4)
-            {
-                ok1=0;
-                ok2=0;
-            }
-            else if(m==1)
-            {
-                cout << "Suma celor 6 numere este: ";
-                if(n==1)
-                    cout << myCalc.adunareNumereIntregi();
-                else
-                    cout << myCalc.adunareNumereReale();
-                cout << endl;
-            }
-            else if(m==2)
-            {
-                cout << "Diferenta dintre primul numar si suma celorlalte 5 numere este: ";
-                if(n==1)
-                    cout << myCalc.scadereNumereIntregi();
-                else
-                    cout << myCalc.scadereNumereReale();
-                cout << endl;
-            }
-            else if(m==3)
-            {
-                cout << "Produsul celor 6 numere este: ";
-                if(n==1)
-                    cout << myCalc.inmultireNumereIntregi();
-                else
-                    cout << myCalc.inmultireNumereReale();
-                cout << endl;
-            }
-            else if(m==4)
-            {
-                cout << "Catul dintre primul numar si produsul celorlalte 5 numere este: ";
-                if(n==1)
-                    cout << myCalc.impartireNumereIntregi();
-                else
-                    cout << myCalc.impartireNumereReale();
-                cout << endl;
-            }
-            else if(m==0)
-                ok2=0;
-        }
+        cout<<"intregi: ";
+        cin>>a>>b>>c>>d>>e>>f;
+        myCalc.declarareNumereIntregi(a,b,c,d,e,f);
     }
+    else if(n==2)
+    {
+        cout<<"reale: ";
+        cin>>g>>h>>i>>j>>k>>l;
+        myCalc.declarareNumereReale(g,h,i,j,k,l);
+    }
+    else
+        goto etexit;
+calculare:
+    cout<<"Introduceti numarul 0 pentru reintroducerea numerelor"<<endl;
+    cout<<"Introduceti numarul 1 pentru adunarea celor 6 numere"<<endl;
+    cout<<"Introduceti numarul 2 pentru scaderea primului numar cu suma celorlalte 5 numere"<<endl;
+    cout<<"Introduceti numarul 3 pentru inmultirea celor 6 numere"<<endl;
+    cout<<"Introduceti numarul 4 pentru impartirea primului numar cu produsul celorlalte 5 numere"<<endl;
+    cout<<"Introduceti orice alt numar intreg pentru terminarea programului"<<endl;
+    cout<<"Numarul introdus este: ";
+    int m;
+    cin>>m;
+    if(m==0)
+        goto inceput;
+    if(m==1)
+    {
+        cout << "Suma celor 6 numere este: ";
+        if(n==1)
+            cout << myCalc.adunareNumereIntregi();
+        else
+            cout << myCalc.adunareNumereReale();
+        cout << endl;
+        goto calculare;
+    }
+    else if(m==2)
+    {
+        cout << "Diferenta dintre primul numar si suma celorlalte 5 numere este: ";
+        if(n==1)
+            cout << myCalc.scadereNumereIntregi();
+        else
+            cout << myCalc.scadereNumereReale();
+        cout << endl;
+        goto calculare;
+    }
+    else if(m==3)
+    {
+        cout << "Produsul celor 6 numere este: ";
+        if(n==1)
+            cout << myCalc.inmultireNumereIntregi();
+        else
+            cout << myCalc.inmultireNumereReale();
+        cout << endl;
+        goto calculare;
+    }
+    else if(m==4)
+    {
+        cout << "Catul dintre primul numar si produsul celorlalte 5 numere este: ";
+        if(n==1)
+            cout << myCalc.impartireNumereIntregi();
+        else
+            cout << myCalc.impartireNumereReale();
+        cout << endl;
+        goto calculare;
+    }
+etexit:
     cout<<"Programul s-a terminat! Puteti apasa orice tasta pentru a inchide fereastra";
     return 0;
 }
